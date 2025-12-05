e4fbfa3e06fd57907a3d198cb17b360b42963562
pepe1224 <josemauriciogonzalez11@gmail.com>
2025-12-05 13:48:30 +0100

    asdzgbf

---
+++ TEMA_4/prueba_java.1.js
@@ -0,0 +1,17 @@
+//Soy un comentario en línea
+        /*j-----------*/
+        //alert('preuba de codigo');
+        //onsole.log('hola mundo');
+        var mensaje = 'hola mundo'
+        console.log(mensaje);
+        console.log(mensaje);
+        console.log(document);
+        console.log(localStorage);
+
+
+        var esViernes = true;
+        console.log("Es vierne sy queremos irnos" + esViernes);
+        //debugger
+        if(esViernes == true){
+                console.log('Pues si es viernes')
+        }
