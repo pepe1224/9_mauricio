3a7e011dbad698c7e57e627cf1331ad5caa7f048
pepe1224 <josemauriciogonzalez11@gmail.com>
2025-12-05 14:12:41 +0100

    ñlk

--- TEMA_4/prueba_java.1.js
+++ TEMA_4/prueba_java.1.js
@@ -1,17 +1,21 @@
 //Soy un comentario en línea
-        /*j-----------*/
-        //alert('preuba de codigo');
-        //onsole.log('hola mundo');
-        var mensaje = 'hola mundo'
-        console.log(mensaje);
-        console.log(mensaje);
-        console.log(document);
-        console.log(localStorage);
+/*j-----------*/
+//alert('preuba de codigo');
+//onsole.log('hola mundo');
+var mensaje = 'hola mundo'
+console.log(mensaje);
+console.log(mensaje);
+console.log(document);
+console.log(localStorage);
 
 
-        var esViernes = true;
-        console.log("Es vierne sy queremos irnos" + esViernes);
-        //debugger
-        if(esViernes == true){
-                console.log('Pues si es viernes')
-        }
+var esViernes = true;
+console.log("Es vierne sy queremos irnos" + esViernes);
+//debugger
+if (esViernes == true) {
+        console.log('Pues si es viernes')
+}
+
+var nameUser = prompt('Introduzaca su nombre');
+console.log('El nombre de usuario es:' + nameUser);
+document.write('<h1>' + nameUser + '</h1>')
